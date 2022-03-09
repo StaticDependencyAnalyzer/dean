262f5070a0219311f202381d70524f2476eb915d
Federico Barcelona <fede_rico_94@hotmail.com>
2022-03-09 12:52:34 +0100

    lint: Deny unused code

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 #![forbid(unsafe_code)]
-#![deny(clippy::pedantic, clippy::cargo)]
+#![deny(clippy::pedantic, clippy::style, clippy::cargo)]
+#![deny(unused)]
 
 mod cmd;
 mod infra;
@@ -7,7 +8,10 @@ mod pkg;
 
 use std::error::Error;
 use std::fs::File;
+use std::time::Duration;
 
+use crate::http::Client;
+use crate::infra::http;
 use cmd::parse_args;
 use infra::npm::DependencyInfoRetriever;
 use pkg::npm::DependencyReader;
@@ -15,7 +19,8 @@ use pkg::npm::DependencyReader;
 fn main() -> Result<(), Box<dyn Error>> {
     let args = parse_args();
 
-    let retriever = Box::new(DependencyInfoRetriever::default());
+    let http_client = http_client();
+    let retriever = Box::new(DependencyInfoRetriever::new(http_client));
     let reader = DependencyReader::new(retriever);
 
     let file = File::open(&args.lock_file)
@@ -38,4 +43,13 @@ fn main() -> Result<(), Box<dyn Error>> {
     })?;
 
     Ok(())
+}
+
+fn http_client() -> Client {
+    let reqwest_client = reqwest::blocking::Client::builder()
+        .timeout(Duration::from_secs(3))
+        .build()
+        .unwrap();
+
+    http::Client::new(reqwest_client)
 }
