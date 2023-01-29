2713018091253309f5e6bf635ea04ca058b2b352
Fede Barcelona <fede_rico_94@hotmail.com>
2023-01-29 23:49:10 +0100

    ci: Update pr lifetime span in test

--- src/infra/repo_contribution.rs
+++ src/infra/repo_contribution.rs
@@ -193,9 +193,9 @@ mod tests {
             .unwrap();
 
         let two_minutes_in_seconds = 2.0 * 60.0;
-        let three_days_in_seconds = 3.0 * 24.0 * 60.0 * 60.0;
+        let a_week_in_seconds = 7.0 * 24.0 * 60.0 * 60.0;
         assert!(pr_lifespan > two_minutes_in_seconds);
-        assert!(pr_lifespan < three_days_in_seconds);
+        assert!(pr_lifespan < a_week_in_seconds);
     }
 
     fn mock_issue_store() -> Box<dyn IssueStore> {
